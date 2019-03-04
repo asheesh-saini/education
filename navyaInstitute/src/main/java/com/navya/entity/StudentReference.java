0cc1e6744cf56f41a42ee6b74b5a29497e411df4
asheesh saini <asheesh@metadesignsolutions.co.uk>
2019-03-04 17:40:09 +0530

    Reference details added

---
+++ navyaInstitute/src/main/java/com/navya/entity/StudentReference.java
@@ -0,0 +1,22 @@
+package com.navya.entity;
+
+public class StudentReference {
+	private String referenceId;
+	private int count;
+
+	public String getReferenceId() {
+		return referenceId;
+	}
+
+	public void setReferenceId(String referenceId) {
+		this.referenceId = referenceId;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+}
--- navyaInstitute/src/main/java/com/navya/repoitory/StudentsRepositoryImpl.java
+++ navyaInstitute/src/main/java/com/navya/repoitory/StudentsRepositoryImpl.java
@@ -12,6 +12,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.navya.entity.StudentEnity;
+import com.navya.entity.StudentReference;
 
 @Repository
 public class StudentsRepositoryImpl implements StudentsRepository {
@@ -32,13 +33,13 @@ public class StudentsRepositoryImpl implements StudentsRepository {
 		public StudentEnity mapRow(ResultSet rs, int rowNum) throws SQLException {
 			// TODO Auto-generated method stub
 			try {
-				if (rs == null) {	
-					System.out.println("rs :"+ rs);
+				if (rs == null) {
+					System.out.println("rs :" + rs);
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
+
 			StudentEnity student = new StudentEnity();
 			// Address address = new Address();
 			student.setId(rs.getString("id"));
@@ -69,23 +70,12 @@ public class StudentsRepositoryImpl implements StudentsRepository {
 
 	@Override
 	public String addStudent(StudentEnity student) {
+		boolean flag = false;
+		int result = 0;
 		String sql = "INSERT INTO STUDENTS (ID,NAME,DOB,MOB,BATCHID,REFERENCE) VALUES (:id,:name,:dob,:mob,:batchId,:reference)";
-		// create a thread
-		Thread t = new Thread(() -> {
-			String sql1 = "SELECT * FROM STUDENTS WHERE ID = :reference";
-			Map<String, Object> paramRef = new HashMap();
-			System.out.println("reference : "+student.getReference());
-			paramRef.put("reference", student.getReference());
-			try {
-			jdbcTemplate.queryForObject(sql, paramRef, new StudentMapper());
-			}
-			catch(Exception e) {
-				//set the flag to indicate whether to insert the data to reference table or not if 0 not otherwise yes.
-				System.out.println("Student Id does not exist");
-				e.printStackTrace();
-			}
-		});
-		t.start();
+		// check reference is is valid or not
+		flag = checkReferenceId(student.getReference());
+		System.out.println("flag as: " + flag);
 		Map<String, Object> param = new HashMap();
 		param.put("id", student.getId());
 		param.put("dob", student.getDob());
@@ -93,15 +83,86 @@ public class StudentsRepositoryImpl implements StudentsRepository {
 		param.put("mob", student.getMob());
 		param.put("name", student.getName());
 		param.put("reference", student.getReference());
-		int result = jdbcTemplate.update(sql, param);
+		if (flag == true) {
+			Thread t = new Thread(() -> {
+				// increase the reference count in reference table
+				checkReference(student.getReference());
+				// updateReferenceInformation();
+			});
+			t.start();
+			result = jdbcTemplate.update(sql, param);
+		}
 		if (result == 0)
 			try {
-				throw new Exception("some problems");
+				throw new Exception("Student does not exist with id " + student.getReference());
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		return result + "";
+	}
+
+	private void checkReference(String reference) {
+		String sql = "SELECT * FROM REFERENCE WHERE REFERENCEID = :reference";
+		String sql1 = null;
+		StudentReference ref = null;
+		int count = 0;
+		// boolean flag = false;
+		Map<String, Object> param = new HashMap();
+		param.put("reference", reference);
+		try {
+			ref = jdbcTemplate.queryForObject(sql, param, new StudentRefMapper());
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		
+		if (ref != null) {
+			count = ref.getCount() + 1;
+			param.put("count", count);
+			System.out.println("count :" + count);
+			System.out.println("reference : "+reference);
+			sql1 = "UPDATE REFERENCE SET COUNT = :count WHERE REFERENCEID = :reference ";
+			jdbcTemplate.update(sql1, param);
+			return;
+		}
+		// update the count to reference table...
+		param.put("count", count);
+		sql1 = "INSERT INTO REFERENCE (REFERENCEID,COUNT) VALUES (:reference,:count)";
+		jdbcTemplate.update(sql1, param);
+	}
+
+	private final class StudentRefMapper implements RowMapper<StudentReference> {
+
+		@Override
+		public StudentReference mapRow(ResultSet rs, int rowNum) throws SQLException {
+			// TODO Auto-generated method stub
+			StudentReference ref = new StudentReference();
+			ref.setReferenceId(rs.getString("referenceId"));
+			ref.setCount(rs.getInt("count"));
+			return ref;
+		}
+
+	}
+
+	private boolean checkReferenceId(String reference) {
+		// fetch all information from the database about students
+		String sql = "SELECT * FROM STUDENTS WHERE ID = :reference";
+		boolean flag = false;
+		Map<String, Object> param = new HashMap();
+		param.put("reference", reference);
+		// System.out.println("reference : "+reference);
+		try {
+			if (!reference.isEmpty()) {
+				jdbcTemplate.queryForObject(sql, param, new StudentMapper());
+				flag = true;
+			}
+		} catch (Exception e) {
+			flag = false;
+			System.out.println("student does not exist");
+			e.printStackTrace();
+		}
+		return flag;
 	}
 
 	@Override
